b3cc0d98995b81260b1eb13e1ca8953b34ea6305
vikingviolinist <misopetrik@gmail.com>
2022-09-03 11:10:40 +0200

    🔥 Remove client

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ const port = process.env.PORT || 1234;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
 });
