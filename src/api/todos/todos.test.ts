04b6150430dd5937e5bb882d7bd3d6df1145fe2c
vikingviolinist <misopetrik@gmail.com>
2022-09-04 11:49:09 +0200

    🧪 Finish tests

--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -1,9 +1,18 @@
 import request from 'supertest';
 
 import app from '../../app';
+import { Todos } from './todos.model';
+
+beforeAll(async () => {
+  try {
+    await Todos.drop();
+  } catch (error) {}
+});
+
+let id = '';
 
 describe('GET /api/v1/todos', () => {
-  it('responds with an array of todos', async () => {
+  it('responds with an array of todos', async () =>
     request(app)
       .get('/api/v1/todos')
       .set('Accept', 'application/json')
@@ -11,9 +20,122 @@ describe('GET /api/v1/todos', () => {
       .expect(200)
       .then((response) => {
         expect(response.body).toHaveProperty('length');
-        expect(response.body.length).toBe(1);
-        expect(response.body[0]).toHaveProperty('content');
-        expect(response.body[0]).toHaveProperty('done');
-      });
+        expect(response.body.length).toBe(0);
+      }));
+});
+
+describe('POST /api/v1/todos', () => {
+  it('responds with an error if the todo is invalid', async () =>
+    request(app)
+      .post('/api/v1/todos')
+      .set('Accept', 'application/json')
+      .send({
+        content: '',
+      })
+      .expect('Content-Type', /json/)
+      .expect(422)
+      .then((response) => {
+        expect(response.body).toHaveProperty('message');
+      }));
+
+  it('responds with the inserted object', async () =>
+    request(app)
+      .post('/api/v1/todos')
+      .set('Accept', 'application/json')
+      .send({
+        content: 'Learn TypeScript',
+        done: false,
+      })
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toHaveProperty('_id');
+        id = response.body._id;
+        expect(response.body).toHaveProperty('content');
+        expect(response.body.content).toBe('Learn TypeScript');
+        expect(response.body).toHaveProperty('done');
+      }));
+});
+
+describe('GET /api/v1/todos/:id', () => {
+  it('responds with a 422 error', (done) => {
+    request(app)
+      .get('/api/v1/todos/qwfp')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422, done);
+  });
+
+  it('responds with a 404 error', (done) => {
+    request(app)
+      .get('/api/v1/todos/6313933e37eba5d55aa8f6ce')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
+
+  it('responds with a single todo', async () =>
+    request(app)
+      .get(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('_id');
+        expect(response.body._id).toBe(id);
+        expect(response.body).toHaveProperty('content');
+        expect(response.body.content).toBe('Learn TypeScript');
+        expect(response.body).toHaveProperty('done');
+      }));
+});
+
+describe('PUT /api/v1/todos/:id', () => {
+  it('responds with an invalid ObjectId error', (done) => {
+    request(app)
+      .put('/api/v1/todos/qwfp')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422, done);
+  });
+
+  it('responds with a single todo', async () =>
+    request(app)
+      .put(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .send({
+        content: 'Learn TypeScript',
+        done: true,
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('_id');
+        expect(response.body._id).toBe(id);
+        expect(response.body).toHaveProperty('content');
+        expect(response.body.content).toBe('Learn TypeScript');
+        expect(response.body).toHaveProperty('done');
+        expect(response.body.done).toBe(true);
+      }));
+});
+
+describe('DELETE /api/v1/todos/:id', () => {
+  it('responds with a 404 error', (done) => {
+    request(app)
+      .delete('/api/v1/todos/6313933e37eba5d55aa8f6ce')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
+
+  it('responds with a 204 status code', (done) => {
+    request(app).delete(`/api/v1/todos/${id}`).expect(204, done);
+  });
+
+  it('responds with a 404 error', (done) => {
+    request(app)
+      .delete(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
   });
 });
