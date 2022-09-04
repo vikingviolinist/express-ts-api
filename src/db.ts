9abe114b9f2b7855838b3f91452bf4ed23c97b7f
vikingviolinist <misopetrik@gmail.com>
2022-09-04 11:46:39 +0200

    🔧 Change local port

--- src/db.ts
+++ src/db.ts
@@ -5,9 +5,8 @@ dotenv.config();
 
 const MONGO_URI =
   process.env.NODE_ENV === 'test'
-    ? 'mongodb://localhost:2717/todo-api'
-    : (process.env.MONGO_URI as string);
+    ? process.env.MONGO_URI_TEST
+    : process.env.MONGO_URI;
 
-console.log('MONGO', MONGO_URI);
-export const client = new MongoClient(MONGO_URI);
+export const client = new MongoClient(MONGO_URI as string);
 export const db = client.db();
