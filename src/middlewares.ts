ae6ed4e0e26d3449cdf17b7ffad959248eae903b
vikingviolinist <misopetrik@gmail.com>
2022-09-04 11:48:43 +0200

    🥅 Add validator middleware

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 import ErrorResponse from './interfaces/ErrorResponse';
+import RequestValidators from './interfaces/RequestValidators';
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
@@ -9,11 +11,38 @@ export function notFound(req: Request, res: Response, next: NextFunction) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response<ErrorResponse>,
+  next: NextFunction
+) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
+}
+
+export function validateRequest(validators: RequestValidators) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (req.params && validators.params) {
+        req.params = await validators.params.parseAsync(req.params);
+      }
+      if (req.body && validators.body) {
+        req.body = await validators.body.parseAsync(req.body);
+      }
+      if (req.query && validators.query) {
+        req.query = await validators.query.parseAsync(req.query);
+      }
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(422);
+      }
+      next(error);
+    }
+  };
 }
